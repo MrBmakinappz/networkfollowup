316f5c31efdec0812fdc82103012c5a37295cba9
MrBmakinappz <mrbrondorcrypto@gmail.com>
2025-12-30 16:43:26 +0100

    Delete backend directory

--- backend/routes/admin.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/auth');
-const { requireAdmin } = require('../middleware/admin');
-router.get('/stats', authenticateToken, requireAdmin, (req, res) => res.json({ users: 100, revenue: 5000 }));
-module.exports = router;
